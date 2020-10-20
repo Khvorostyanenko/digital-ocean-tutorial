e140268fe7c68d5ca5ec13eb1ef8cfcb3dacdd2d
Marat Dospolov <marat.dospolov@gmail.com>
2020-10-20 23:46:41 +0300

    fix for react useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
-import "./index.css";
-import DivTogglerReact from "./DivTogglerReact.js";
-import DivTogglerRedux from "./DivTogglerRedux.js";
+import React from 'react'
+import './index.css'
+import DivTogglerReact from './DivTogglerReact'
+import DivTogglerRedux from './DivTogglerRedux'
+// расширение js файлов указывать не надо
+// import DivTogglerRedux from './DivTogglerRedux.js'
 
 const App = () => (
   <div>
     <DivTogglerRedux />
     <DivTogglerReact />
   </div>
-);
+)
 
-export default App;
+export default App
--- src/DivTogglerReact.js
+++ src/DivTogglerReact.js
@@ -1,25 +1,31 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
-export default function DivTogglerReact(initialValue = false) {
-  const { showAlert } = useState(false);
-  const { hideAlert } = useState(true);
+const DivTogglerReact = () => {
+  const [alertVisible, setAlertVisible] = useState(false)
   return (
     <div className="p-10">
-      <div className="bg-green-400 p-10 mb-3">Alert content</div>
+      <div
+        className="bg-green-400 p-10 mb-3"
+        style={{ display: alertVisible ? 'block' : 'none' }}
+      >
+        Alert content
+      </div>
       <button
         type="button"
-        onClick={() => showAlert(true)}
+        onClick={() => setAlertVisible(true)}
         className="bg-gray-500 p-4 mr-2"
       >
         Show alert
       </button>
       <button
         type="button"
-        onClick={() => hideAlert(false)}
+        onClick={() => setAlertVisible(false)}
         className="bg-gray-500 p-4"
       >
         Hide alert
       </button>
     </div>
-  );
+  )
 }
+
+export default DivTogglerReact
